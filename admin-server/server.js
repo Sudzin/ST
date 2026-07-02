fcf1a3064c2c681c5bbfc2f4efcb4ba2a7fae611
Sudzin <148689071+Sudzin@users.noreply.github.com>
2026-07-02 13:26:29 +0300

    сервер администрирования разворачивается на 3001 порту + добавлена логика опроса состояния сервера администрирования для фронта

--- admin-server/server.js
+++ admin-server/server.js
@@ -4,7 +4,9 @@ const FILE_SERVER_URL = "http://localhost:3000/api/ping";
 const INTERVAL_MS = 100000; // чтобы не заполняло консоль
 const ADMIN_SERVER_PORT = 3001;
 
-console.log(`Начинаю пинговать файловый сервер: ${FILE_SERVER_URL}\n`);
+let adminServerOnline = false;
+
+//console.log(`Начинаю пинговать файловый сервер: ${FILE_SERVER_URL}\n`);
 
 function checkFileServer() {
   http
@@ -27,5 +29,35 @@ function checkFileServer() {
 }
 
 setInterval(checkFileServer, INTERVAL_MS);
-
 checkFileServer();
+
+const adminServer = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5174");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
+  if (req.url === "/api/health" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    res.end(JSON.stringify({ status: "ok" }));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+adminServer.listen(ADMIN_SERVER_PORT, () => {
+  console.log(
+    `[Administration server] Работает на http://localhost:${ADMIN_SERVER_PORT}`,
+  );
+  console.log(`Начинаю пинговать файловый сервер: ${FILE_SERVER_URL}\n`);
+});
+
+//setInterval(checkFileServer, INTERVAL_MS);
+//checkFileServer();
