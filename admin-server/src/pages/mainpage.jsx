bef76a82ca66b48ade00f46dd3491e4c6003f0c7
Sudzin <m43228273@gmail.com>
2026-07-04 13:26:59 +0300

    добавтл логику отправки данныз о статистике на фронт

--- admin-server/src/pages/mainpage.jsx
+++ admin-server/src/pages/mainpage.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function MainPage() {
+  const [stats, setStats] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+
+    fetch("http://localhost:3001/api/admin/stats", {
+      headers: { Authorization: `Bearee ${token}` },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setStats(data);
+      })
+      .catch((err) => {
+        console.error("Не удалось загрузить статистику: ", err);
+        setError("Не удалось загрузить статистику");
+      });
+  }, []);
+
   return (
     <div
       style={{
