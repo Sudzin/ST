b6b5954c1b14001ec49ec7bc25c812f6092fca89
Sudzin <m43228273@gmail.com>
2026-07-05 13:10:20 +0300

    поправил запуск с учётом тестов

---
+++ admin-server/scripts/devWithTests.js
@@ -0,0 +1,27 @@
+const { spawn } = require("child_process");
+const waitOn = require("wait-on");
+
+const server = spawn("npx", ["nodemon", "server.js"], {
+  stdio: "inherit",
+  shell: true,
+});
+
+waitOn({ resources: ["http://localhost:3001/api/health"], timeout: 15000 })
+  .then(() => {
+    console.log("\n[Tests] Сервер готов, запускаю тесты...\n");
+    const tests = spawn("npm", ["test"], { stdio: "inherit", shell: true });
+
+    tests.on("exit", (code) => {
+      console.log(
+        `\n[Tests] Тесты завершены (код ${code}). Сервер продолжает работать.\n`,
+      );
+    });
+  })
+  .catch((err) => {
+    console.error("[Tests] Сервер не поднялся вовремя:", err.message);
+  });
+
+process.on("SIGINT", () => {
+  server.kill();
+  process.exit();
+});
